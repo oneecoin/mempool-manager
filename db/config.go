2be06c386f1e3493b3b00d150ecda778494503e0
Onee_Only <kimww0306@gmail.com>
2023-02-10 17:01:39 +0900

    MongoDB 구성

--- db/config.go
+++ db/config.go
@@ -1,6 +1,38 @@
 package db
 
-type DBConfig struct {
+import (
+	"context"
+	"os"
 
+	"github.com/onee-only/mempool-manager/lib"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	DB_NAME       = "oneecoin-mempool"
+	EXAMPLE_CHAIN = "example-chain"
+	TRANSACTONS   = "transactions"
+)
+
+type Database struct {
+	ExampleChain *mongo.Collection
+	Transactions *mongo.Collection
 }
 
+var DB = &Database{}
+
+func InitDatabase() *mongo.Client {
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		panic("URI does not exist")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	lib.HandleErr(err)
+
+	DB.ExampleChain = client.Database(DB_NAME).Collection(EXAMPLE_CHAIN)
+	DB.Transactions = client.Database(DB_NAME).Collection(TRANSACTONS)
+
+	return client
+}
--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/onee-only/mempool-manager/api"
+	"github.com/onee-only/mempool-manager/db"
 )
 
 func main() {
+
+	// gin
 	api.InitServer()
+
+	// .env
+	if os.Getenv("DEBUG") == "" {
+		if err := godotenv.Load(); err != nil {
+			fmt.Println("No .env file found")
+		}
+	}
+
+	// MongoDB
+	client := db.InitDatabase()
+	defer client.Disconnect(context.TODO())
 }
