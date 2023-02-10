01508102487519c807de6bf6054c79f785b91b37
Onee_Only <kimww0306@gmail.com>
2023-02-10 15:31:27 +0900

    지갑 생성 작성

--- api/services/wallet-service.go
+++ api/services/wallet-service.go
@@ -1,1 +1,26 @@
 package services
+
+import (
+	"github.com/onee-only/mempool-manager/api/models"
+)
+
+type walletService interface {
+	New() *models.Wallet
+}
+
+type WalletService struct{}
+
+var Wallet walletService = WalletService{}
+
+func (WalletService) New() *models.Wallet {
+
+	wallet := &models.Wallet{}
+
+	privateKey := models.CreatePrivateKey()
+	publicKey := models.GetPublicKey(privateKey)
+
+	wallet.PublicKey = publicKey
+	wallet.SetPrivateKey(privateKey)
+
+	return wallet
+}
