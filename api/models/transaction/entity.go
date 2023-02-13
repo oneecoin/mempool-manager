d7a11a41b9febde9a929044b43a715ef2f332340
Onee_Only <kimww0306@gmail.com>
2023-02-13 13:20:55 +0900

    Transaction을 위한 Stringer interface 구현

--- api/models/transaction/entity.go
+++ api/models/transaction/entity.go
@@ -1,19 +1,45 @@
 package transaction_model
 
+import "fmt"
+
 type TxIn struct {
 	TxID      string `json:"txId"`
 	Index     int    `json:"index"`
 	Signature string `json:"signature"`
 }
-
 type TxOut struct {
 	PublicKey string `json:"publicKey"`
 	Amount    int    `json:"amount"`
 }
 
+type TxInS []*TxIn
+type TxOutS []*TxOut
+
 type Tx struct {
-	ID        string   `json:"id"`
-	Timestamp int      `json:"timestamp"`
-	TxIns     []*TxIn  `json:"txIns"`
-	TxOuts    []*TxOut `json:"txOuts"`
+	ID        string `json:"id"`
+	Timestamp int    `json:"timestamp"`
+	TxIns     TxInS  `json:"txIns"`
+	TxOuts    TxOutS `json:"txOuts"`
+}
+
+func (txIns *TxInS) String() string {
+	s := "["
+	for i, txIn := range *txIns {
+		if i > 0 {
+			s += ", "
+		}
+		s += fmt.Sprintf("%v", txIn)
+	}
+	return s + "]"
+}
+
+func (txOuts *TxOutS) String() string {
+	s := "["
+	for i, txOut := range *txOuts {
+		if i > 0 {
+			s += ", "
+		}
+		s += fmt.Sprintf("%v", txOut)
+	}
+	return s + "]"
 }
