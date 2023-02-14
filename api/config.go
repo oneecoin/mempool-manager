e98c12334d6bb91433d2fdae4b519f0c2454f9e5
Onee_Only <kimww0306@gmail.com>
2023-02-14 21:56:05 +0900

    Time zone 설정

--- api/config.go
+++ api/config.go
@@ -1,12 +1,14 @@
 package api
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/onee-only/mempool-manager/api/routes"
 )
 
 func InitServer() {
-
+	os.Setenv("TZ", "Asia/Seoul")
 	app := gin.Default()
 
 	// routes
