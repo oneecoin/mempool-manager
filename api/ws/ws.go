acd1f103553875f9b7672f1f62d6857aea40b7ad
Onee_Only <kimww0306@gmail.com>
2023-02-16 13:44:03 +0900

    peers count response

--- api/ws/ws.go
+++ api/ws/ws.go
@@ -65,4 +65,7 @@ func UpgradeWS(c *gin.Context) {
 	// add this connection to peers map
 	prs.BroadcastNewPeer(p)
 	prs.InitPeer(p)
+	c.JSON(http.StatusOK, struct{ PeersCount int }{
+		PeersCount: len(prs.V),
+	})
 }
